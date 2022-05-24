b0159be1e20dc28194523f05834d2009741c0b68
Hrzán Ondřej <ondatra@centrum.cz>
2022-05-24 15:32:56 +0200

    about, contact

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import Link from "next/link"
 
 function Navbar() {
   return (
     <div className='text-white fixed top-0 w-screen flex justify-end items-center h-14 mx-[-100px]'>
+      <Link href="/">
         <p className='px-5 text-xl h-full hover:bg-gray-800 flex items-center cursor-pointer duration-300'>Home</p>
+      </Link>
+      <Link href="/about">
         <p className='px-5 text-xl h-full hover:bg-gray-800 flex items-center cursor-pointer duration-300'>About</p>
+      </Link>
+      <Link href="/contact">
         <p className='px-5 text-xl h-full hover:bg-gray-800 flex items-center cursor-pointer duration-300'>Contact</p>
+      </Link>
     </div>
   )
 }
