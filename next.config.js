31831a1c1c10437f70ba518324286003eea8310a
Hrzán Ondřej <ondatra@centrum.cz>
2022-05-27 15:18:08 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -2,11 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['apod.nasa.gov'],
-    loader:"akamai",
-    path:""
+    domains: ["apod.nasa.gov"],
+    loader: "akamai",
+    path: "",
   },
+  basePath: "/spejs-nextjs",
+  assetPrefix: "/spejs-nextjs",
+};
 
-}
-
-module.exports = nextConfig
+module.exports = nextConfig;
